f9a98e3d1d5b97912aed0f5a8b5a28d00faf4b33
Clovis Wanderley Bertholini Sobrinho <clovis.cwbs@gmail.com>
2021-07-07 10:04:50 -0400

    now its printing the number

--- print_rax.asm
+++ print_rax.asm
@@ -1,24 +1,22 @@
 ; DEV: CLOVIS WANDERLEY BERTHOLINI SOBRINHO
 ; APP: print_rax (shows the value on register rax)
 ; DATE: July, 6 of 2021.
-section .data                                                           
-codes:                              
-    db      '0123456789ABCDEF'      
+section .data                 
+newline_char: db 10                                          
+codes: db '0123456789ABCDEF'      
 
 section .text                       
 global _start                       
 _start:                         
     ; number 1122... in hexadecimal format
-    ;mov rax, 0x1122334455667788    
-    
+    mov rax, 0x1122334455667788   
     mov rdi, 1                  
     mov rdx, 1                  
     mov rcx, 64   
 	; Each 4 bits should be output as one hexadecimal digit
 	; Use shift and bitwise AND to isolate them
 	; the result is the offset in 'codes' array
-_start.loop:        
-    mov rax, 0x1122334455667788                    
+_start.loop:                            
     push rax                    
     sub rcx, 4
 	; cl is a register, smallest part of rcx
@@ -43,6 +41,13 @@ _start.loop:
     test rcx, rcx
     jnz _start.loop   
     
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, newline_char
+    mov rdx, 1
+    syscall
+
+
     mov     rax, 60            ; invoke 'exit' system call
     xor     rdi, rdi
     syscall
